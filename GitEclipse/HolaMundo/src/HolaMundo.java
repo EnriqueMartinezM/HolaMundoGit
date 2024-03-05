d87cf18cf3f356c43058902c7c36bdcecc6b59ca
EnriqueMartinezM <162356315+EnriqueMartinezM@users.noreply.github.com>
2024-03-05 18:39:13 +0100

    Add files via upload
    Primeros archivos

---
+++ GitEclipse/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,15 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double Numero=5;
+
+int Dato=3;
+
+System.out.println("2 cambio -Consola");
+
+String manin="Matias";
+	}
+
+}
